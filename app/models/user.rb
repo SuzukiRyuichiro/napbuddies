49d9db9cce77e40c9244bb0da2d49ab316f58203
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-22 18:00:55 +0900

    added seed. also modified default valu of max_guest as it was causing erros in creating instances

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :events, through: :bookings
   has_many :events_as_host, class_name: 'Event'
 
-
+  validates :username, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
