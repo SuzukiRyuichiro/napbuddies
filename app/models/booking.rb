cf6b66189107d6c06342437fdd8bd280b04ce373
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-22 14:02:15 +0900

    added some connections

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  enum status: { pending: 0, approved: 1, declined: 2 }
+  enum role: { big_spoon: 0, little_spoon: 1 }
 end
