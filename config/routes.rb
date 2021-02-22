116d1799dccf5e2eb4ee2cbe95aa44f87d6a51a8
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-23 08:20:28 +0900

    generated a eventscontoller with new create index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'events/new'
+  get 'events/create'
+  get 'events/index'
+  get 'events/show'
   devise_for :users
   root to: 'pages#home'
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
