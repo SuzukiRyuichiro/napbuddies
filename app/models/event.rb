0edf657df429f49766ff59e329b99f5380dce699
Ken Funakoshi <funaks@live.com>
2021-02-22 16:20:56 +0900

    added validations and description to events table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :title, presence: true, :location, presence: true
+  validates :time, presence: true, :max_guest, presence: true
+  validates :description, presence: true
 end
