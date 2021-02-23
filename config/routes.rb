3dfc8c9519a3e5b097e835f1bab98fc86b8c7d89
Ken Funakoshi <funaks@live.com>
2021-02-23 15:05:05 +0900

    added index styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/events', to: 'events#index', as: 'events'
+  # get '/events', to: 'events#index', as: 'events'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/events/new', to: 'events#new', as: 'new_event'
