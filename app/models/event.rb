0a48d07de5daae7739b8a14404f709d033c8bb60
Ken Funakoshi <funaks@live.com>
2021-02-24 13:07:31 +0900

    added map and icons

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
   validates :title, presence: true
   validates :location, presence: true
