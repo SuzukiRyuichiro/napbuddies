3b9a3b01dc4b4c285cf78e1e46d7768e5f6a5bf3
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-25 15:47:46 +0900

    now the only the host can decline or approve of the request '

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,4 +4,16 @@ class BookingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def update?
+    record.event.user == user
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
 end
