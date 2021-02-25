c9145ea3ec1918d14bfd186369c6893defa484db
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-26 01:41:28 +0900

    added validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   # belongs_to :reviewed, class_name: "User", foreign_key: :user_id
+
+  validates :rating, inclusion: { in: (0..5).to_a, message: "Rating has to be between 0 to 5" }
 end
