801e1e473324e0e6396ea11f91eeff01b0e0ed23
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-25 16:41:13 +0900

    validates whether the host is the one trying to book to oneselve's event

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,17 @@
+class BookingValidator < ActiveModel::Validator
+  def validate(record)
+    if record.event.user == record.user
+      record.errors[:base] << "Host cannot book his or her own event"
+    end
+  end
+end
+
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
   enum status: { pending: 0, approved: 1, declined: 2 }
   enum spoon: { no_pref: 0, little: 1, mid: 2, big: 3 }
+
+  validates_with BookingValidator
 end
