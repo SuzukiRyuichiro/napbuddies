e3d2bccd5c994874e71b56c8ec5ebb4cdd1a34e6
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-26 16:07:08 +0900

    corrected some redirect_to that was workign weird

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,7 +12,7 @@ class BookingsController < ApplicationController
     @booking.event = @event
     authorize @booking
     if @booking.save
-      redirect_to events_path
+      redirect_to dashboard_path
     else
       render 'new'
     end
