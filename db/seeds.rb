c7adb3e0244ff6a0dcff304a0d9329619ce9c748
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-26 13:40:52 +0900

    now the dashboard shows write a review link if the booking is approved and already event has finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,17 @@
 
 
 20.times do
-user = User.new(username: Faker::Twitter.screen_name, email:Faker::Internet.email, password: Faker::Creature::Animal.name,
+user = User.new(username: Faker::Twitter.screen_name, email:Faker::Internet.email, password: "password",
 bio:Faker::TvShows::BojackHorseman.quote, first_name:Faker::Movies::LordOfTheRings.character, last_name:Faker::Creature::Animal.name)
 user.save || next
 end
+
 20.times do
 review = Review.new(content: Faker::TvShows::RickAndMorty.quote, rating: rand(3..5))
 review.reviewer = User.all.sample
 review.reviewee = User.all.sample
 review.save || next
+puts "#{review.reviewer.username} wrote #{review.content} to #{review.reviewee.username}"
 end
 
 puts "created #{User.count} users!"
@@ -28,7 +30,7 @@ puts "created #{User.count} users!"
   event = Event.new(
     title: Faker::Book.title,
     location: Faker::Address.state,
-    time: rand(5.days).seconds.from_now,
+    time: rand(5.days).seconds.ago,
     user: User.all.sample,
     max_guest: rand(2..3),
     description: Faker::Quote.yoda,
@@ -42,11 +44,12 @@ puts "created #{User.count} users!"
 end
 
 Event.all.each do |event|
-  3.times do
+  5.times do
     booking = Booking.new(
       event: event,
       user: User.all.sample,
-      spoon: rand(0..3)
+      spoon: rand(0..3),
+      status: rand(0..2)
     )
     if booking.save
       puts "#{booking.user.username} requested a nap with #{booking.event.user.username}"
